1cbae9d4a64e7172efaad5151ee575f1f4b60df5
oUnknown0 <ounknown039@gmail.com>
2023-11-14 01:53:34 +0800

    Update test.py

--- web/test/test.py
+++ web/test/test.py
@@ -14,42 +14,45 @@ chrome_options.add_argument('--disable-gpu')
 
 # Set up the WebDriver with Chrome options
 # driver = webdriver.Chrome(options=chrome_options)
-driver = webdriver.Remote(options=chrome_options)
+driver = webdriver.Remote(
+    command_executor='http://192.168.1.4:4444/wd/hub',
+    options=chrome_options
+)
 
 try:
     # Open the webpage
-    # driver.get('http://192.168.1.4:5500/web/pages/homepage.html')
-    driver.get('https://www.google.com')
+    driver.get('http://192.168.1.4:5500/web/pages/homepage.html')
+    # driver.get('https://www.google.com')
 
-#     # Explicit wait for the search input
-#     search_input = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.ID, 'search'))
-#     )
+    # Explicit wait for the search input
+    search_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'search'))
+    )
 
-#     # Find and interact with elements
-#     search_input.send_keys('hello')
+    # Find and interact with elements
+    search_input.send_keys('hello')
 
-#     submit_button = driver.find_element(By.ID, 'submit')
-#     submit_button.click()
+    submit_button = driver.find_element(By.ID, 'submit')
+    submit_button.click()
 
-#     # Explicit wait for the success message
-#     success_message = WebDriverWait(driver, 10).until(
-#         EC.visibility_of_element_located((By.ID, 'success'))
-#     )
+    # Explicit wait for the success message
+    success_message = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, 'success'))
+    )
 
-#     # Perform assertions to validate the results
-#     assert 'hello' in success_message.text, "Success message does not contain the expected text."
-#     print(f"Message: {str(success_message.text)}")
+    # Perform assertions to validate the results
+    assert 'hello' in success_message.text, "Success message does not contain the expected text."
+    print(f"Message: {str(success_message.text)}")
 
 
-# except TimeoutException as te:
-#     print(f"Timeout occurred: {str(te)}")
-# except NoSuchElementException as nse:
-#     print(f"Element not found: {str(nse)}")
-# except AssertionError as ae:
-#     print(f"Assertion error: {str(ae)}")
-# except Exception as e:
-#     print(f"An unexpected error occurred: {str(e)}")
+except TimeoutException as te:
+    print(f"Timeout occurred: {str(te)}")
+except NoSuchElementException as nse:
+    print(f"Element not found: {str(nse)}")
+except AssertionError as ae:
+    print(f"Assertion error: {str(ae)}")
+except Exception as e:
+    print(f"An unexpected error occurred: {str(e)}")
 
 finally:
     # Close the browser
