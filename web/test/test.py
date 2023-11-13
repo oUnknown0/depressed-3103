83c2e207731139a5a2efdb874cd9e2db6ca46480
oUnknown0 <ounknown039@gmail.com>
2023-11-14 02:48:13 +0800

    Update test.py

--- web/test/test.py
+++ web/test/test.py
@@ -44,6 +44,7 @@ try:
 
     # Perform assertions to validate the results
     assert 'hello' in success_message.text, "Success message does not contain the expected text."
+    print("Correct Input Test:", success_message.text)
 
 except TimeoutException as te:
     raise CustomWebDriverException(f"Timeout occurred: {str(te)}")
@@ -53,6 +54,65 @@ except AssertionError as ae:
     raise CustomWebDriverException(f"Assertion error: {str(ae)}")
 except Exception as e:
     raise CustomWebDriverException(f"An unexpected error occurred: {str(e)}")
+
+try:
+    # Open the webpage
+    driver.get('http://192.168.1.4:5500/web/pages/homepage.html')
+    # driver.get('https://www.google.com')
+
+    # Explicit wait for the search input
+    search_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'search'))
+    )
+
+    # Find and interact with elements
+    search_input.send_keys('105 OR 1=1')
+
+    submit_button = driver.find_element(By.ID, 'submit')
+    submit_button.click()
+
+    # Explicit wait for the success message
+    success_message = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, 'failure'))
+    )
+
+    # Perform assertions to validate the results
+    assert 'Invalid characters in the search term. Please enter a valid search term.' in success_message.text, "Failure message does not contain the expected text."
+    print("SQL Injection Test:", success_message.text)
+
+except TimeoutException as te:
+    raise CustomWebDriverException(f"Timeout occurred: {str(te)}")
+except NoSuchElementException as nse:
+    raise CustomWebDriverException(f"Element not found: {str(nse)}")
+except AssertionError as ae:
+    raise CustomWebDriverException(f"Assertion error: {str(ae)}")
+except Exception as e:
+    raise CustomWebDriverException(f"An unexpected error occurred: {str(e)}")
+
+try:
+    # Open the webpage
+    driver.get('http://192.168.1.4:5500/web/pages/homepage.html')
+    # driver.get('https://www.google.com')
+
+    # Explicit wait for the search input
+    search_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'search'))
+    )
+
+    # Find and interact with elements
+    search_input.send_keys('<script>alert(123)</script>')
+
+    submit_button = driver.find_element(By.ID, 'submit')
+    submit_button.click()
+
+    # Explicit wait for the success message
+    success_message = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, 'failure'))
+    )
+
+    # Perform assertions to validate the results
+    assert 'Invalid characters in the search term. Please enter a valid search term.' in success_message.text, "Failure message does not contain the expected text."
+    print("XSS Attack Test:", success_message.text)
 
 finally:
     # Close the browser
